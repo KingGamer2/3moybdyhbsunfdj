0d3f017a891a56f4401619b9a24dab8f7aba5dba
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-09-23 04:14:17 +0300

    Add files via upload

---
+++ dndn.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "--"
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+=================
+فديتك يا قلبييء ممكن تدخل السيرفر
+الدعوة خاصة لك
+https://discord.gg/fcNTZC
+
+ ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+=================
+فديتك يا قلبييء ممكن تدخل السيرفر
+الدعوة خاصة لك
+https://discord.gg/fcNTZC
+
+ ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
